4b0762b9017170f5d2fa825f55a11ac639ed6c55
wldn7601 <wldn76011@gmail.com>
2025-10-27 18:22:41 +0900

    SCM test java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 111;
+        int a = 121;
         int b = 211;
         int sum = a + b;
         System.out.println(a+" + "+ b + sum);
