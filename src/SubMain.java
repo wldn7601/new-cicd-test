e1e23891504106b26d4da74289d1f975086337d9
wldn7601 <wldn76011@gmail.com>
2025-10-27 18:34:49 +0900

    log java

---
+++ src/SubMain.java
@@ -0,0 +1,8 @@
+public class SubMain {
+    public static void main(String[] args) {
+        int a = 122;
+        int b = 211;
+        int sum = a * b;
+        System.out.println(a+" * "+ b +" = " +  sum);
+    }
+}
