879f9c52135b8269f8d5b46405b1649791a99296
wldn7601 <wldn76011@gmail.com>
2025-10-27 17:53:55 +0900

    1 + 2 java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        System.out.println("1 + 2 = " + sum);
+    }
+}
+
